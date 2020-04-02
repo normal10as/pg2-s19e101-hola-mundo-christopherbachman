1edc19118eb97daf5c6e99a9f306465be5b127bb
HP <HP@LAPTOP-UMGMIIJM>
2020-04-01 21:11:38 -0300

    Prueba 2

--- HelloWorld/Program.vb
+++ HelloWorld/Program.vb
@@ -3,7 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Dim currentDate = DateTime.Now
-        Console.WriteLine("What is your name? ")
+        Console.WriteLine("Cual es tu nombre? ")
         Dim name = Console.ReadLine()
         Console.WriteLine($"Hola, {name}, on {currentDate:d} at {currentDate:t}")
     End Sub
